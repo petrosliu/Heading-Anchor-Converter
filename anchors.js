5ffcd2557defd52b9bd65818183f65576608349b
petrosliu <petros.ydliu@gmail.com>
2016-05-12 16:34:43 -0500

    v0.2

--- anchors.js
+++ anchors.js
@@ -1,27 +1,37 @@
 function convert(){
     var lines=document.getElementById("code").value.split('\n');
-    var codediff=[],resultdiff=[];
+    var consoletext=[];
     var pass=false;
+    var hmap={};
     for(var i=0;i<lines.length;i++){
         var line=lines[i];
         if(/^\s*```/i.test(line)) pass=!pass;
         if(!pass && /^\s*#+/i.test(line)){
-            if(/^\s*#+\s*\[.*\].*\(.*\).*/i.test(line)) continue;
-            var s=0,e=line.length-1;
-            while(/[\s#]/i.test(line[s])) s++;
-            while(/[\s]/i.test(line[e])) e--;
+            if(/^\s*#+\s*\[.*\].*\(#.*\).*/i.test(line)){
+                consoletext.push('SKIP:\t'+'L'+(i+1)+'\t'+line);
+                continue;
+            }
+            var s=-1,e=line.length;
+            while(/[\s#]/i.test(line[++s]));
+            while(/[\s]/i.test(line[--e]));
             var heading=line.substring(s, e+1);
-            var anchor='(#'+heading.toLowerCase().replace(/[^a-z0-9-\s]/g,'').replace(/\s/g, "-")+')';
-            s=0;
-            while(/[\s]/i.test(line[s])) s++;
-            while(/[#]/i.test(line[s])) s++;
-            codediff.push(lines[i]);
-            lines[i]=line.substring(0, s)+' ['+heading+']'+anchor;
-            resultdiff.push(lines[i]);
+            var anchor='#'+heading.toLowerCase().replace(/[^a-z0-9-\s]/g,'').replace(/\s/g, "-");
+            s=-1;
+            while(/[\s]/i.test(line[++s]));
+            while(/[#]/i.test(line[++s]));
+            var logline='L'+(i+1)+'\t'+lines[i];
+            lines[i]=line.substring(0, s)+' ['+heading+']('+anchor+')';
+            logline+=' => '+lines[i];
+            if(anchor in hmap || /[\[\]\(\)]+/i.test(heading) || /[\[\]\(\)]+/i.test(anchor)){
+                if(anchor in hmap) consoletext.push('WARN:\t'+logline+"\tThe anchor is not unique. L"+hmap[anchor]);
+                if(/[\[\]\(\)]+/i.test(heading)||/[\[\]\(\)]+/i.test(anchor)) consoletext.push('WARN:\t'+logline+"\tSpecial characters exist.");
+            }
+            else consoletext.push('PASS:\t'+logline);
+            if(!(anchor in hmap)) hmap[anchor]=i+1;
         }
     }
+    consoletext.push('Something wrong? Please report it in https://github.com/petrosliu/Heading-Anchor-Converter/issues');
     document.getElementById("result").value = lines.join('\n');
-    document.getElementById("codediff").value = codediff.join('\n');
-    document.getElementById("resultdiff").value = resultdiff.join('\n');
+    document.getElementById("console").value = consoletext.join('\n');
     window.location = '#result';
 };
